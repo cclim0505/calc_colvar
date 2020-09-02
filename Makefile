17fa83461a4fe84f146e054da3b4cf6d1843e7a5
cclim0505 <cclim.0505@gmail.com>
2020-09-02 17:23:09 +0800

    added feature to calculate a series of structures 	new file:   .gitignore 	modified:   Makefile 	renamed:    README -> README.md 	modified:   main.f 	modified:   metacncv.f 	modified:   metargcv.f 	new file:   sample.xyz

--- Makefile
+++ Makefile
@@ -9,8 +9,8 @@ CALCFUNC = func_calc_meta.f
 all: colvar
 
 colvar : $(MAIN) $(METACN) $(METARG) $(CALCFUNC)
-	$(FC) $(MAIN) $(METACN) $(METARG) $(CALCFUNC) -o x.colvar
+	$(FC) $(MAIN) $(METACN) $(METARG) $(CALCFUNC) -o colvar.out
 
 clean:
-	rm x.colvar
+	rm colvar.out
 	rm output_colvar.dat
--- main.f
+++ main.f
@@ -5,30 +5,37 @@
       REAL(KIND=8)      :: CN_val,RG_val
       INTEGER           :: j1,j2,istat
       INTEGER           :: atoms
+      INTEGER           :: timestep
+      INTEGER           :: f_xyz, f_cv
+      INTEGER           :: ierr
+      INTEGER           :: counter=0
       REAL(KIND=8),ALLOCATABLE  :: x(:)
+
       CN_val = 0.0
       RG_val = 0.0
+
       WRITE(*,'(/A)') repeat("*",80)
       WRITE(*,*) 'Colvar Calculation Programme for'
       WRITE(*,*) 'Coordination Number, CN'
       WRITE(*,*) '           &                 '
       WRITE(*,*) ' Radius of Gyration, Rg'
+      WRITE(*,*) '(Works for constant atom number)'
       WRITE(*,'(/A)') repeat("*",80)
 
       CALL get_command_argument(1,file_name)
       IF(file_name =='') THEN
         WRITE(*,*)
         WRITE(*,*) 'Use'
-        WRITE(*,*) './x.colvar file_name'
+        WRITE(*,*) './colvar.out file_name'
         WRITE(*,*) 'Try again'
         WRITE(*,*)
         STOP
       END IF
 !=======================================================
 ! Determine the number of atoms
-      OPEN(20,file=trim(file_name),status='old')
-      READ(20,*) atoms
-      CLOSE(20)
+      OPEN(NEWUNIT=f_xyz,FILE=trim(file_name),STATUS='old')
+      READ(f_xyz,*) atoms
+      CLOSE(f_xyz)
 
 !=======================================================
 ! Display file and number of atoms
@@ -38,38 +45,54 @@
 ! Allocate arrays
       ALLOCATE(x(atoms*3),STAT=istat)
 
+      OPEN(NEWUNIT=f_cv,FILE='output_colvar.dat',STATUS='replace')
+      WRITE(f_cv,*) 'timestep, CN, Rg'
 !=======================================================
 ! Read coordinates
-      OPEN(22,file=trim(file_name),status='old')
-      READ(22,*) 
-      READ(22,*) 
-      DO j1=1,atoms
-         j2=j1*3
-         READ(22,*) dummy, x(j2-2), x(j2-1), x(j2)
+      OPEN(NEWUNIT=f_xyz,FILE=trim(file_name),STATUS='old')
+
+      DO
+        READ(f_xyz,*,IOSTAT=ierr) 
+        IF (ierr /= 0) EXIT
+        READ(f_xyz,*) dummy, dummy, dummy, timestep
+        DO j1=1,atoms
+           j2=j1*3
+           READ(f_xyz,*) dummy, x(j2-2), x(j2-1), x(j2)
+        END DO
+  
+        CALL calc_colvar_CN_val(atoms,x,CN_val)  
+        CALL calc_colvar_RG_val(atoms,x,RG_val)  
+  
+        WRITE(f_cv,*) timestep, CN_val, RG_val
+        counter = counter + 1
       END DO
 
-!=======================================================
-! Calculate colvar
-      CALL calc_colvar_CN_val(atoms,x,CN_val)  
-      CALL calc_colvar_RG_val(atoms,x,RG_val)  
+
+      CLOSE(f_cv)
+      CLOSE(f_xyz)
+
+
+
+
 
 !=======================================================
-! Output
-      OPEN(21,file='output_colvar.dat',status='replace')
-      WRITE(21,*) 'CN=', CN_val
-      WRITE(21,*) 'RG=', RG_val
-      CLOSE(21)
-      CLOSE(22)
-
-      WRITE(*,*) 'Colvars calculated:'
-      WRITE(*,*) 
-      WRITE(*,*) 'CN=', CN_val
-      WRITE(*,*) 'RG=', RG_val
+! DEBUGGING STARTS
+!=======================================================
+!      WRITE(*,*) 'Colvars calculated:'
+!      WRITE(*,*) 
+!      WRITE(*,*) 'Timestep', timestep
+!      WRITE(*,*) 'CN=', CN_val
+!      WRITE(*,*) 'RG=', RG_val
+!=======================================================
+! DEBUGGING ENDS
+!=======================================================
 
 
       WRITE(*,*) 
       WRITE(*,*) 'Results saved in:'
       WRITE(*,*) 'output_colvar.dat'
+      WRITE(*,*) 
+      WRITE(*,*) 'Total CVs calculated:', counter
       WRITE(*,*) 
 
       END PROGRAM main
